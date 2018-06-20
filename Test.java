a124a813d746f201f6c98795c10632cd48edfc5c
Fitzgerald Afful <fitzafful@gmail.com>
2018-06-20 11:55:18 +0000

    Surname
    Signed-off-by: Fitzgerald Afful <fitzafful@gmail.com>

--- Test.java
+++ Test.java
@@ -1,6 +1,12 @@
 class Names{
 
+    String surname;
+
     public static void main(String[] args) {
         println("Kofi is a boy");
+    }
+
+    public String getName(){
+        return surname;
     }
 }
